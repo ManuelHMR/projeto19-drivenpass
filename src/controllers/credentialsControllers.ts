b9124e58b3ecd8f24fa905aba0968553023e2893
ManuelHMR <manuelhmrjf@gmail.com>
2022-07-17 20:44:04 -0300

    feat: get all credentials

---
+++ src/controllers/credentialsControllers.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { createCredentialsServices, getCredentialsServices, getCrendentialByIdServices } from "../services/credentialServices";
+
+export async function credentialController(req: Request, res: Response) {
+    const { userId } = res.locals; 
+    await createCredentialsServices(req.body, parseInt(userId));
+    res.sendStatus(201);
+};
+
+export async function getCredentialsController(req: Request, res: Response) {
+    const { userId } = res.locals;
+    const result =  await getCredentialsServices(parseInt(userId));
+    return res.send(result);
+};
+
+export async function getCredentialByIdController(req: Request, res: Response) {
+    const { userId } = res.locals;
+    const credentialId = req.params.id;
+    const result = await getCrendentialByIdServices(parseInt(userId), parseInt(credentialId));
+    return res.send(result);
+};
