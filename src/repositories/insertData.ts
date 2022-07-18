b50f1cfa1f45255efcd4b7a20d9ba5492aa73da1
ManuelHMR <manuelhmrjf@gmail.com>
2022-07-18 17:20:25 -0300

    fix: interfaces

--- src/repositories/insertData.ts
+++ src/repositories/insertData.ts
@@ -1,7 +1,7 @@
-import {  Notes, Wifis } from "@prisma/client";
+import {  Cards, Credentials, Notes, Wifis } from "@prisma/client";
 import { prisma } from "../config/database";
 
-export async function insertData(data: Omit<Wifis | Notes, "id">, table : "credentials" | "notes" | "cards" | "wifis"){
+export async function insertData(data: Omit<Wifis | Notes | Credentials | Cards, "id">, table : "credentials" | "notes" | "cards" | "wifis"){
     return await prisma[table as any].create({
             data
     });
